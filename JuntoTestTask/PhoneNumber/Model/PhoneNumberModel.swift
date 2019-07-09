da681b5f6acfda42ef7840e641b6ad729f1398af
Ацамаз Бицоев <atsamazbitsoev@yandex.ru>
2019-07-09 22:56:38 +0300

    commit

---
+++ JuntoTestTask/PhoneNumber/Model/PhoneNumberModel.swift
@@ -0,0 +1,9 @@
+//
+//  PhoneNumberModel.swift
+//  JuntoTestTask
+//
+//  Created by Ацамаз Бицоев on 09/07/2019.
+//  Copyright © 2019 Ацамаз Бицоев. All rights reserved.
+//
+
+import Foundation
---
+++ JuntoTestTask/PhoneNumber/View/PhoneNumberView.swift
@@ -0,0 +1,19 @@
+//
+//  PhoneNumberView.swift
+//  JuntoTestTask
+//
+//  Created by Ацамаз Бицоев on 09/07/2019.
+//  Copyright © 2019 Ацамаз Бицоев. All rights reserved.
+//
+
+import UIKit
+
+class PhoneNumberView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
