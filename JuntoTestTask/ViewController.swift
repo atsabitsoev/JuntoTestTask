2ff61228bb8fdc707c170bf2fd16d3ac1cc58a98
Ацамаз Бицоев <atsamazbitsoev@yandex.ru>
2019-07-09 19:38:37 +0300

    Initial Commit

---
+++ JuntoTestTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JuntoTestTask
+//
+//  Created by Ацамаз Бицоев on 09/07/2019.
+//  Copyright © 2019 Ацамаз Бицоев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
