b62bec6bbbf15f4a6bcc4f4c3ec608fe4e1b1250
Ацамаз Бицоев <atsamazbitsoev@yandex.ru>
2019-07-10 23:09:44 +0300

    commit

--- JuntoTestTask/Servicies/DBService.swift
+++ JuntoTestTask/Servicies/DBService.swift
@@ -19,22 +19,5 @@ class DBService {
     static func registerUser(phone: String,
                              firstName: String,
                              lastName: String) {
-        
-        do {
-            let db = try Connection("path/to/db.sqlite3")
-            
-            let users = Table("users")
-            let id = Expression<Int64>("id")
-            let name = Expression<String?>("name")
-            let email = Expression<String>("email")
-            
-            try db.run(users.create { t in
-                t.column(id, primaryKey: true)
-                t.column(name)
-                t.column(email, unique: true)
-            })
-        } catch {
-            print(error.localizedDescription)
-        }
     }
 }
